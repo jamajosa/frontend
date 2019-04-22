ec48fbd192b282b2049d282b6ea271f8612672db
jamajosa <jhuijgens1@live.nl>
2019-04-22 23:14:22 +0200

    i hope for the best

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-const express = require('express');
-const app = express();
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-frontend'));
+app.use(express.static(__dirname + '/dist/GameDb'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/my-frontend/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/GameDb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
