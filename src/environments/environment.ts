0dd7abb20de258e00a7674626bde0e637af08dc2
jamajosa <jhuijgens1@live.nl>
2019-04-22 21:57:24 +0200

    i hope for the best

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiBaseUrl:'https://toolbackend.herokuapp.com/api'
+  production: true,
+  apiBaseUrl:'http://toolbackend.herokuapp.com/api'
 };
 
 /*
