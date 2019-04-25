e170444cd8efe5194c070cf29f08a9b352d0c822
jamajosa <jhuijgens1@live.nl>
2019-04-26 00:25:36 +0200

    i hope for the best

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/GameDb'));
+app.use(express.static(__dirname + '/dist/my-frontend'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/GameDb/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/my-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
